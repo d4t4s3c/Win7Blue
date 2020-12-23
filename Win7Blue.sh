af71495b979422b9df47ccc80ac6e7442da3c82e
Miguel R <d4t4s3c@protonmail.com>
2020-12-23 14:37:25 +0100

    update

--- Win7Blue.sh
+++ Win7Blue.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#Author: d4t4s3c
-#Email:  d4t4s3c@protonmail.com
-#GitHub: www.github.com/d4t4s3c
+#Author:  d4t4s3c
+#Twitter: @d4t4s3c
+#Email:   d4t4s3c@protonmail.com
+#GitHub:  www.github.com/d4t4s3c
 
 #colors
 b="\033[1;37m"
